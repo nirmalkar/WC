ae73d44211b5d7af646bf37646ae5237300beade
Hemant Nirmalkar <nirmalkar@Hemants-MacBook-Pro.local>
2021-11-06 21:16:20 +0530

    toggle, animation and style for info modal

--- shadowDOM/PopUp.js
+++ shadowDOM/PopUp.js
@@ -3,9 +3,11 @@ const template = document.createElement('template')
 template.innerHTML = `
     <link rel="stylesheet" href="./css/popup.css">
     <div class="popup-container">
+    <div class="popup-button">
     <div class="avatar-container">
-    <img class="avatar" src="https://avatars.githubusercontent.com/u/50173564?v=4" alt="avatar"/>
+    <img class="avatar" alt="avatar"/>
     <div class="name">Hemant Nirmalkar</div>
+    </div>
     </div>
         <div class="popup-content">
         <div class="popup-header">
@@ -15,9 +17,19 @@ template.innerHTML = `
         <div class="line2"></div>
         </div>
         </div>
-        <div class"row instagram"></div>
-        <div class="row twitter"></div>
+        <a class="social-link instagram" href="https://www.instagram.com/monty_davinci/" rel="noreferrer noopener" target="_blank">
+        <div class="row">
+        <img class="instagram-img" src="./images/instagram.svg" alt="instagram"/>
+        <div class="social-id">@montydavinci</div>
         </div>
+        </a>
+        <a class="social-link twitter" href="https://twitter.com/nirmalkar_" rel="noreferrer" target="_blank">
+        <div class="row">
+        <img class="twitter-img" src="./images/twitter.svg" alt="twitter"/>
+        <div class="social-id">@nirmalkar_</div>
+        </div>
+        </div>
+        </a>
     </div>
 
 `
@@ -28,6 +40,30 @@ class PopUp extends HTMLElement {
         this.showModal = false
         this.attachShadow({ mode: 'open' })
         this.shadowRoot.appendChild(template.content.cloneNode(true))
+        this.shadowRoot.querySelector('.avatar').src =
+            this.getAttribute('avatar')
+    }
+
+    toggleModalVisible() {
+        let modalDisplay =
+            this.shadowRoot.querySelector('.popup-content').style.display
+        const isModalHidden = modalDisplay === '' || modalDisplay === 'none'
+        if (isModalHidden) {
+            this.shadowRoot.querySelector('.popup-content').style.display =
+                'block'
+        } else {
+            this.shadowRoot.querySelector('.popup-content').style.display =
+                'none'
+        }
+    }
+
+    connectedCallback() {
+        this.shadowRoot
+            .querySelector('.close')
+            .addEventListener('click', this.toggleModalVisible.bind(this))
+        this.shadowRoot
+            .querySelector('.popup-button')
+            .addEventListener('click', this.toggleModalVisible.bind(this))
     }
 }
 
