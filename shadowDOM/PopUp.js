5d900d53a82bc19b8dd31d5159cb63ed6a0c808a
Hemant Nirmalkar <nirmalkar@Hemants-MacBook-Pro.local>
2021-11-06 19:33:51 +0530

    modal header css

--- shadowDOM/PopUp.js
+++ shadowDOM/PopUp.js
@@ -4,10 +4,19 @@ template.innerHTML = `
     <link rel="stylesheet" href="./css/popup.css">
     <div class="popup-container">
     <div class="avatar-container">
-    <img class="avatar" src="https://avatars2.githubusercontent.com/u/12097?s=460&v=4" alt="avatar"/>
+    <img class="avatar" src="https://avatars.githubusercontent.com/u/50173564?v=4" alt="avatar"/>
     <div class="name">Hemant Nirmalkar</div>
     </div>
         <div class="popup-content">
+        <div class="popup-header">
+        <img class="header-img" src="https://avatars.githubusercontent.com/u/50173564?v=4" alt="avatar"/>
+        <div class="close">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        </div>
+        </div>
+        <div class"row instagram"></div>
+        <div class="row twitter"></div>
         </div>
     </div>
 
